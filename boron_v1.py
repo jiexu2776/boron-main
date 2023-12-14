19613514ab9f13ab821d20e6af8a4c46651483e7
Jie Xu <107989499+jiexu2776@users.noreply.github.com>
2023-12-14 17:03:45 +0100

    Update boron_v1.py

--- boron_v1.py
+++ boron_v1.py
@@ -23,7 +23,7 @@ def add_logo():
         """
         <style>
             [data-testid="stSidebarNav"] {
-                background-image: url(https://raw.githubusercontent.com/jiexu2776/boron-main/main/images/website-profile.gif?token=GHSAT0AAAAAACLTWTPTX37H6H5ZTMIKCPDEZL3EEUA);
+                background-image: url(https://raw.githubusercontent.com/jiexu2776/boron-main/main/images/website-profile.gif);
                 background-repeat: no-repeat;
                 padding-top: 120px;
                 background-position: 100px 20px;
@@ -61,7 +61,7 @@ st.markdown(
 )
 
 st.sidebar.image(
-    'https://raw.githubusercontent.com/jiexu2776/boron-main/main/images/Goethe-Logo.gif?token=GHSAT0AAAAAACLTWTPSXR2JP5GSYBONEDBGZL3EDUA')
+    'images/Goethe-Logo.gif')
 
 
 
