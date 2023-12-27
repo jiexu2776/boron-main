bc2e3d7e8d5e50de927953f6b1169b973b5c2250
jiexu2776 <jie.xu@stud.uni-frankfurt.de>
2023-12-27 13:38:03 +0100

    Update 02_(2) Data reduction.py

--- pages/02_(2) Data reduction.py
+++ pages/02_(2) Data reduction.py
@@ -257,6 +257,7 @@ with col1:
 
 with col2:
     st.session_state.df_data= pd.DataFrame(st.session_state.df_data)
+    st.write(st.session_state.df_data)
     list_try = [[i] for i in st.session_state.df_data['filename']]
     
     st.session_state.df_data['file name'] = selSmpType(st.session_state.df_data['filename'])
